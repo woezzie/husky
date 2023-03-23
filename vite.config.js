1dd4369a0d78fe85b74b4473c264b72562754671
Ernst Bolt <er.bolt@windesheim.nl>
2023-03-23 15:08:41 +0100

    security sri branch toegevoegd

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,21 @@
-export default {
-  root: 'my-app'
-};
+import html from '@rollup/plugin-html';
+import sri from 'rollup-plugin-sri';
+import { defineConfig } from 'vite';
+
+// export default {
+//   root: 'my-app',
+//   plugins: {
+//
+//
+//   }
+// };
+
+export default defineConfig({
+  root: 'my-app',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      plugins: [html(), sri()]
+    }
+  }
+});
